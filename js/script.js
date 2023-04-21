ffcfdabc9070292292630bbc09d2a8a3cc66b6d0
danilosantana240765 <danilosantana240765@gmail.com>
2023-04-21 08:09:55 -0300

    Melhoria na interface

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const api_url = 'https://www.google.com/speech-api/v1/synthesize?enc=mpeg&lang=pt-br&client=lr-language-tts&use_google_only_voices=1'
+const api_url = 'https://www.google.com/speech-api/v1/synthesize?enc=mpeg&client=lr-language-tts&use_google_only_voices=1'
 const textarea = document.querySelector('textarea#text')
-const audio = document.querySelector('audio')
-const btn = document.querySelector('button')
+const idioma = document.querySelector('select')
 const range = document.querySelector("input[type='range']")
+const btn = document.querySelector('button')
+const audio = document.querySelector('audio')
 
 btn.addEventListener("click", () => {
     const query = textarea.value.trim()
+    const language = idioma.value
     const speed = Number(range.value) / 10
     
     if(query != "") {
-        audio.src = `${api_url}&speed=${speed}&&text=${query}`
+        audio.src = `${api_url}&speed=${speed}&lang=${language}&text=${query}`
 
         audio.onloadeddata = () => {
             audio.play()
